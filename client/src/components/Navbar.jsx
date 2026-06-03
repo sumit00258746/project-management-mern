6b6ca8984195c5d5ea8a57578ca32ca18a054a09
Sumit Mondal <sumit@cludotechnology.in>
2026-06-04 01:32:23 +0530

    2

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,6 +43,7 @@ const Navbar = ({ setIsSidebarOpen }) => {
                         }
                     </button>
 
+
                     {/* User Button */}
                     <UserButton />
                 </div>
